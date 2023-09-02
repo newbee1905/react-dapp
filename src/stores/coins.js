d7efca799b948ce2094595946ae83be8d1b01eab
newbee1905 <beenewminh@outlook.com>
2023-09-02 13:35:56 +1000

    feat: add data and store to get full data

---
+++ src/stores/coins.js
@@ -0,0 +1,43 @@
+import data from '@/data.json'
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+export default create(
+	persist(
+		(set, get) => ({
+			raw: Object.freeze(data),
+			data: {},
+			api: (apiEndPoint) => `https://api.gemini.com/v2/ticker/${apiEndPoint}`,
+			getData: async () => {
+				const changed = localStorage.getItem('coins-data-changed')
+				if (changed) {
+					if (Date.now() - changed < 60 * 1000) {
+						return set({})
+					}
+				}
+				localStorage.setItem('coins-data-changed', Date.now())
+
+				let data = {}
+				for (const [key, value] of Object.entries(get().raw)) {
+					let coinValues = await (await fetch(get().api(value.api))).json()
+
+					coinValues.diff =
+						(coinValues.changes[0] - coinValues.changes[23]) /
+						coinValues.changes[0]
+					if (!coinValues.diff) coinValues.diff = 0
+					console.log(coinValues.diff)
+					coinValues.diff *= 100
+
+					data[key] = {
+						...value,
+						values: coinValues,
+					}
+				}
+				set({ data: data })
+			},
+		}),
+		{
+			name: 'coins-data',
+		}
+	)
+)
