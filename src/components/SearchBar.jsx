198e3e181619e53ed5fc3e2cdb4bc82b71c49ac5
newbee1905 <beenewminh@outlook.com>
2023-09-03 12:13:56 +1000

    chore: use tabler unocss icon instead

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -19,23 +19,7 @@ export default function SearchBar({ input, setInput }) {
 					p="l-3"
 					className="pointer-events-none"
 				>
-					<svg
-						w="4"
-						h="4"
-						text="slate-300"
-						aria-hidden="true"
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 20 20"
-					>
-						<path
-							stroke="currentColor"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth="2"
-							d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-						/>
-					</svg>
+					<i text="slate-200 xl" i="tabler-search" />
 				</div>
 				<input
 					type="search"
